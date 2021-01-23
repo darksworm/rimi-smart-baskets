5d2da88f72137f9797f6a1a424557b1e1d01a0f8
ilmars <9987548+darksworm@users.noreply.github.com>
2021-01-23 19:28:30 +0200

    tests for DOM with empty basket

--- test/test.js
+++ test/test.js
@@ -1,20 +1,28 @@
 const chai = require('chai')
 const fs = require('fs')
-const {JSDOM, VirtualConsole} = require('jsdom')
+const {JSDOM} = require('jsdom')
 require('mock-local-storage')
+const axios = require("axios");
+const AxiosMockAdapter = require("axios-mock-adapter")
+const {asyncTasks} = require('await-async-task')
+
+before(function () {
+    global.axiosMock = new AxiosMockAdapter(axios);
+    global.axios = axios;
+})
 
 function setupDOM(htmlPath) {
     const mockCartHTML = fs.readFileSync(htmlPath, 'utf-8')
-
-    const mockConsole = new VirtualConsole();
     const dom = new JSDOM(mockCartHTML, {
-        'url': 'https://www.rimi.lv/e-veikals/lv/checkout/cart',
-        mockConsole
+        'url': 'https://www.rimi.lv/e-veikals/lv/checkout/cart'
     })
 
     global.document = dom.window.document
     global.window = dom.window
     global.window.localStorage = global.localStorage
+
+    global.axiosMock.resetHandlers()
+    global.axiosMock.resetHistory()
 
     const userscriptJS = fs.readFileSync('index.js', 'utf-8')
     global.window.eval(userscriptJS)
@@ -42,11 +50,12 @@ describe('DOM with opened saved basket', function () {
     }
 
     beforeEach(function () {
+        localStorage.clear()
         setupDOM('test/rimi-cart-saved-cart-opened.html')
     })
 
-    it('shouldn\'t alter localStorage on inject', function () {
-        chai.assert.isEmpty(global.localStorage);
+    it('shouldn\'t write to localStorage on inject', function () {
+        chai.assert.isEmpty(localStorage);
     });
 
     it('should contain `Save in "Smart Baskets"` button', function () {
@@ -67,7 +76,7 @@ describe('DOM with opened saved basket', function () {
 
     it('should create record in localstorage when save button clicked', function () {
         saveCurrentCart();
-        chai.assert.isNotEmpty(global.localStorage.carts);
+        chai.assert.isNotEmpty(localStorage.carts);
     });
 
     it('should store correct product count when save button clicked', function () {
@@ -100,5 +109,102 @@ describe('DOM with opened saved basket', function () {
         let afterCartProductCount = getFirstCart().products.length;
 
         chai.assert.equal(afterCartProductCount, initialCartProductCount - 1);
+    })
+})
+
+describe('DOM with empty basket', function () {
+    function getCartBtn() {
+        return document.querySelector('.saved-cart-popup button[name="cart"]');
+    }
+
+    function getCartAppendBtn() {
+        return getCartBtn().children[0];
+    }
+
+    beforeEach(function () {
+        let carts = {
+            '13371337': {
+                id: '13371337',
+                name: 'dankmemes',
+                products: [{
+                    "id": "227060",
+                    "name": "Skābais krējums Exporta 25% 360g",
+                    "category": "SH-11-4-12/SH-11-4/SH-11/SH",
+                    "brand": "Baltais",
+                    "price": 1.27,
+                    "currency": "EUR",
+                    "quantity": 1,
+                    "hiddenAmount": "1",
+                    "hiddenStep": "1"
+                }, {
+                    "id": "210673",
+                    "name": "Pilnpiena biezpiens Talsu 9% 300g",
+                    "category": "SH-11-1-1/SH-11-1/SH-11/SH",
+                    "brand": "Talsu",
+                    "price": 1.32,
+                    "currency": "EUR",
+                    "quantity": 1,
+                    "hiddenAmount": "1",
+                    "hiddenStep": "1"
+                }, {
+                    "id": "223984",
+                    "name": "Sviests Exporta 82,5% 200g",
+                    "category": "SH-11-7-17/SH-11-7/SH-11/SH",
+                    "brand": "Baltais Exporta",
+                    "price": 2.15,
+                    "currency": "EUR",
+                    "quantity": 1,
+                    "hiddenAmount": "1",
+                    "hiddenStep": "1"
+                }]
+            }
+        };
+
+        localStorage.clear()
+        localStorage.setItem('carts', JSON.stringify(carts));
+        setupDOM('test/rimi-cart-empty.html')
+
+        axiosMock
+            .onPut("https://www.rimi.lv/e-veikals/cart/change")
+            .reply(200, {});
+    })
+
+    it('should send a request for each product when appending a stored cart', async function () {
+        getCartAppendBtn().click();
+        await asyncTasks(100);
+
+        chai.expect(axiosMock.history.put.length).to.equal(3);
+    })
+
+    it('should send requests which match rimi API format', async function () {
+        getCartAppendBtn().click();
+        await asyncTasks(100);
+
+        for (let request of axiosMock.history.put) {
+            let data = JSON.parse(request.data);
+            chai.assert.hasAllKeys(data, ['_method', '_token', 'amount', 'step', 'product'])
+        }
+    })
+
+    it('should use the correct CSRF token', async function () {
+        getCartAppendBtn().click();
+        await asyncTasks(100);
+
+        for (let request of axiosMock.history.put) {
+            let data = JSON.parse(request.data);
+            chai.assert.equal(data["_token"], "redacted");
+            chai.assert.equal(request.headers["x-csrf-token"], "redacted");
+        }
+    })
+
+    it('doesn\'t die when append cart button is spammed', async function() {
+        let clickCount = 100;
+        for (let i = 0; i < clickCount; i++) {
+            getCartAppendBtn().click();
+        }
+
+        await asyncTasks(100);
+
+        chai.expect(axiosMock.history.put.length).to.equal(3 * clickCount);
     })
 })
