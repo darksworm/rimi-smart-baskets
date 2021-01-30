1cd1d326e3a1f3665237a0c1aac2c7685eae1a7f
ilmars <9987548+darksworm@users.noreply.github.com>
2021-01-30 13:23:02 +0200

    clean up unittests

--- test/unit.js
+++ test/unit.js
@@ -1,53 +1,56 @@
-import chai from "chai";
+import {expect} from "chai";
+import {before, beforeEach, describe, it} from "mocha";
+
 import {JSDOM} from "jsdom";
-import {asyncTasks} from "await-async-task";
+
 import RimiDOM from "../src/lib/rimi/rimiDOM";
 import RimiAPI from "../src/lib/rimi/rimiAPI";
 import CartStorage from "../src/lib/cart/cartStorage"
-import LoadingIndicator from "../src/lib/ui/loadingIndicator"
 import CartUpdater from "../src/lib/cart/cartUpdater";
+
+import LoadingIndicator from "../src/lib/ui/loadingIndicator"
 
 describe('RimiDOM with blank page and google.com as URL', function () {
     let rimiDOM;
     before(function () {
         const dom = new JSDOM(`<input name='_token'/><meta name='csrf-token'/>`, {
             'url': 'https://google.com'
-        })
+        });
         rimiDOM = new RimiDOM(dom.window);
-    })
+    });
 
     describe('getCurrentCart', function () {
         it('should not find products in cart', function () {
             let cart = rimiDOM.getCurrentCart();
-            chai.assert.isEmpty(cart.products);
-        })
+            expect(cart.products).to.be.empty;
+        });
         it('should not find a cart name', function () {
             let cart = rimiDOM.getCurrentCart();
-            chai.assert.isUndefined(cart.name);
-        })
+            expect(cart.name).to.be.a('undefined');
+        });
         it('should not find a cart id', function () {
             let cart = rimiDOM.getCurrentCart();
-            chai.assert.isUndefined(cart.id);
+            expect(cart.id).to.be.a('undefined');
         })
-    })
+    });
 
     describe('isInSavedCart', function () {
         it('should not be truthy', function () {
             let result = rimiDOM.isInSavedCart();
-            chai.assert.isFalse(result);
+            expect(result).to.equal(false);
         })
     })
-})
+});
 
 describe('RimiAPI', function () {
     describe('constructor', function () {
         it('throws without params', function () {
-            chai.assert.throws(() => new RimiAPI());
-        })
+            expect(() => new RimiAPI()).to.throw();
+        });
         it('doesnt throw with three params', function () {
-            new RimiAPI('whatever', 'nextever', {})
-        })
-    })
+            expect(() => new RimiAPI('whatever', 'nextever', {})).to.not.throw();
+        });
+    });
 
     describe('updateProduct', function () {
         it('calls axios.put', function () {
@@ -57,9 +60,9 @@ describe('RimiAPI', function () {
                     called = true
                 }
             };
-            let api = new RimiAPI('whatever', 'nextever', axiosMock)
+            let api = new RimiAPI('whatever', 'nextever', axiosMock);
             api.updateProduct(123, 1, 1);
-            chai.assert.isTrue(called);
+            expect(called).to.equal(true);
         })
     })
 })
@@ -79,26 +82,27 @@ describe('CartStorage', function () {
 
     describe('constructor', function () {
         it('throws when called with empty object argument', function () {
-            chai.assert.throws(() => new CartStorage({}));
-        })
+            expect(() => new CartStorage({})).to.throw();
+        });
+
         it('throws when called without arguments', function () {
-            chai.assert.throws(() => new CartStorage());
-        })
+            expect(() => new CartStorage()).to.throw();
+        });
+
         it('throws when called with object missing setItem', function () {
             let storage = {
-                getItem: () => {
-                }
+                getItem: () => {}
             };
-            chai.assert.throws(() => new CartStorage(storage));
-        })
+            expect(() => new CartStorage(storage)).to.throw();
+        });
+
         it('throws when called with object missing getItem', function () {
             let storage = {
-                setItem: () => {
-                }
+                setItem: () => {}
             };
-            chai.assert.throws(() => new CartStorage(storage));
-        })
-    })
+            expect(() => new CartStorage(storage)).to.throw();
+        });
+    });
 
     describe('getStoredCart', function () {
         let storageMock;
@@ -107,19 +111,19 @@ describe('CartStorage', function () {
         beforeEach(function () {
             storageMock = createStorageMock();
             cartStorage = new CartStorage(storageMock);
-        })
+        });
 
         it('throws when requested cart does not exist', function () {
-            chai.assert.throws(() => cartStorage.getStoredCart(1234));
-        })
+            expect(() => cartStorage.getStoredCart(1234)).to.throw();
+        });
 
         it('returns cart when requested cart exists', function () {
             let stored = {'1234': {'test': 'yes'}};
             storageMock.data.carts = JSON.stringify(stored);
             let retrieved = cartStorage.getStoredCart(1234);
-            chai.assert.deepEqual(retrieved, stored['1234']);
-        })
-    })
+            expect(retrieved).to.deep.equal(stored['1234']);
+        });
+    });
 
     describe('storeCart', function () {
         let storageMock;
@@ -128,16 +132,16 @@ describe('CartStorage', function () {
         beforeEach(function () {
             storageMock = createStorageMock();
             cartStorage = new CartStorage(storageMock);
-        })
+        });
 
         it('stored cart can be retreived with getStoredCart', function () {
             let stored = {'test': 'yes', 'id': 1234};
             cartStorage.storeCart(stored);
             let retrieved = cartStorage.getStoredCart(1234);
-            chai.assert.deepEqual(retrieved, stored);
+            expect(retrieved).to.deep.equal(stored);
         })
     })
-})
+});
 
 describe('LoadingIndicator', function () {
     let dom;
@@ -149,37 +153,37 @@ describe('LoadingIndicator', function () {
         id = 'loader-container';
         indicator = new LoadingIndicator(dom.window.document, id);
         indicator.show();
-    })
+    });
 
     it('show creates element in dom', function () {
         let elem = dom.window.document.getElementById(id);
-        chai.assert.isNotNull(elem);
-    })
+        expect(elem).to.not.be.a('null');
+    });
 
     it('show called twice does not create another element', function () {
         indicator.show();
         let bodyChildCount = dom.window.document.body.childElementCount;
-        chai.assert.equal(bodyChildCount, 1);
-    })
+        expect(bodyChildCount).to.equal(1);
+    });
 
     it('should create element which has a classname', function () {
         let elem = dom.window.document.getElementById(id);
         let className = elem.className;
-        chai.assert.isNotEmpty(className);
-    })
+        expect(className).to.not.be.empty;
+    });
 
     it('should create element which is not empty', function () {
         let elem = dom.window.document.getElementById(id);
-        chai.assert.notEqual(elem.childElementCount, 0);
-    })
+        expect(elem.childElementCount).to.not.equal(0);
+    });
 
     it('updates textcontent of loader when updateText called', function () {
         let text = "this is some random string";
         indicator.updateText(text);
         let elem = dom.window.document.getElementById(id);
-        chai.assert.include(elem.textContent, text);
-    })
-})
+        expect(elem.textContent).to.include(text);
+    });
+});
 
 describe('CartUpdater', function () {
     describe('appendStoredCartItemsToActiveCart', function () {
@@ -190,8 +194,7 @@ describe('CartUpdater', function () {
         beforeEach(function () {
             dom = new JSDOM('');
             api = {
-                updateProduct: function () {
-                }
+                updateProduct: () => {}
             };
             indicator = new LoadingIndicator(dom.window.document);
         });
@@ -203,10 +206,10 @@ describe('CartUpdater', function () {
                     called = true
                 }
             };
-            let cartUpdater = new CartUpdater(api, indicator, dom.window)
+            let cartUpdater = new CartUpdater(api, indicator, dom.window);
             cartUpdater.doAction([]);
-            chai.assert.isFalse(called);
-        })
+            expect(called).to.equal(false);
+        });
 
         it('should make one api call for each stored item', async function () {
             let calledTimes = 0;
@@ -217,13 +220,8 @@ describe('CartUpdater', function () {
                 }
             };
             let cartUpdater = new CartUpdater(api, indicator, dom.window);
-            cartUpdater.doAction(() => {}, [1, 2, 3]);
-            await asyncTasks();
-            chai.assert.equal(calledTimes, 3);
-        })
-    })
-})
-
-after(function () {
-    global.DONT_EXECUTE_USERSCRIPT = undefined
-})
+            await cartUpdater.doAction(() => {}, [1, 2, 3]);
+            expect(calledTimes).to.equal(3);
+        });
+    });
+});
